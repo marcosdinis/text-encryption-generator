bda41aff41222a0af1a7a13402489c6e0a78dbf4
Marcos Dinis <itsmarcosdinis@gmail.com>
2026-04-10 11:26:32 -0300

    feat: implement shift_letter function

--- helper.py
+++ helper.py
@@ -1,16 +1,20 @@
-def get_upper_letter(letter: int) -> str:
-    return
+def shift_letter(letter: int, cifra: int, base: int) -> str:
+    code = letter - base;
+    code += cifra
+    code = code % 26
+    code += base
+    return chr(code)
 
 def encrypt(message: str, cifra: int) -> str:
     text = []
     for char in message:
-        code = 0;
         if not char.isalpha():
             text.append(char)
             continue 
         if char.isupper():
-            text.append(get_upper_letter(char))
-            continue;
+            text.append(shift_letter(ord(char), cifra, 65))
+            continue
+        text.append(shift_letter(ord(char), cifra, 97))
             
     res = "".join(text)
     return res;
